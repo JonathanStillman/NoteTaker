e159b4f1ed5247f3d889f73e4a4d08956bc901fe
Jonathan Stillman <jonathanstillman86@yahoo.com>
2020-10-31 13:03:02 -0500

    Update

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/notes.html"));
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
 app.get("/api/notes", function(req, res) {
@@ -23,7 +23,7 @@ app.get("/api/notes/:id", function(req, res) {
 });
 
 app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+    res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 app.post("/api/notes", function(req, res) {
